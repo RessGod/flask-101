190750dc22ac8931b662378599ce5bc6818d3f7f
Sergino RIBA <serginoriba155@gmail.com>
2022-03-16 14:58:18 +0300

    App Flask

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,8 @@
 # pylint: disable=missing-docstring
 
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,5 +15,7 @@ def produits():
     PRODUCTS = {
         1: { 'id': 1, 'name': 'Skello' },
         2: { 'id': 2, 'name': 'Socialive.tv' },
+        3: { 'id': 3, 'name': 'Le Wagon'}
     }
-    return PRODUCTS
+    products = list(PRODUCTS)
+    return jsonify(products)
